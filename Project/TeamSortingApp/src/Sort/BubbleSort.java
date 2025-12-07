f4fd9599b90d5aff5508936c1c1e16b84dd2570e
“Mr.Jularat_Khoonkham” <jularat.k@kkumail.com>
2025-12-07 13:53:35 +0700

    ตั้งค่าโปรเจคและ สร้างMain, Interface, Mock classes

---
+++ Project/TeamSortingApp/src/Sort/BubbleSort.java
@@ -0,0 +1,15 @@
+package Sort;
+
+import Interface.SortInterface;
+
+public class BubbleSort implements SortInterface {
+    
+    @Override
+    public int[] sort(int[] data) {
+        System.out.println("[Running Bubble Sort...]");
+
+        // Logic
+        
+        return data;
+    }
+}
---
+++ Project/TeamSortingApp/src/Sort/QuickSort.java
@@ -0,0 +1,13 @@
+package Sort;
+
+import Interface.SortInterface;
+
+public class QuickSort implements SortInterface {
+    
+    @Override
+    public int[] sort(int[] data) {
+        System.out.println("[Running Selection Sort...]");
+        // --- เขียน logic selection sort ตรงนี้ ---
+        return data;
+    }
+}
